f26cbb57a1b856116178ef41565a407507302dd9
Wexel <wexel17@yahoo.com>
2017-12-13 08:40:37 -0800

    fix: remove service worker, app is small and it's too quirky

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { Provider } from 'react-redux';
 import './index.css';
 import heroes from './heroes';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import Simulator  from "./simulator";
 import Counter from "./counter";
 
@@ -150,7 +149,6 @@ function reduce(state = INITIAL_STATE, action) {
 let store = createStore(reduce);
 
 ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
-registerServiceWorker();
 
 function advanceSimulation() {
     try {
